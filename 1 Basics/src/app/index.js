cf8e7ffdb3736458b6a8d373028ec9de668f9755
Tejas-Nanaware <nanawaret@hotmail.com>
2018-02-20 19:31:31 +0530

    Added first component navbar

--- 1 Basics/src/app/index.js
+++ 1 Basics/src/app/index.js
@@ -1,1 +1,31 @@
-console.log("works")
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+	render() {
+		// what to do when component renders the code
+		return(
+			<nav className="navbar navbar-inverse navbar-fixed-top">
+			<div className="container-fluid">
+				<div className="navbar-header">
+				<button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span className="icon-bar"></span>
+					<span className="icon-bar"></span>
+					<span className="icon-bar"></span>                        
+				</button>
+				<a className="navbar-brand" href="">React App</a>
+				</div>
+				<div className="collapse navbar-collapse" id="myNavbar">
+				<ul className="nav navbar-nav">
+					<li><a href="" className="active">Home</a></li>
+					<li><a href="">About Us</a></li>
+					<li><a href="">Contact</a></li>
+				</ul>
+				</div>
+			</div>
+		</nav>
+		);
+	}
+}
+
+render(<App/>, window.document.getElementById("nav"));
