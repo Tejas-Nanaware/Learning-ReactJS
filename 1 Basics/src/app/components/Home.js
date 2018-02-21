d9e83f3038f53a3e1eb07b2ec285a96bcecab2ce
Tejas-Nanaware <nanawaret@hotmail.com>
2018-02-22 00:25:22 +0530

    Passing data with props

--- 1 Basics/src/app/components/Home.js
+++ 1 Basics/src/app/components/Home.js
@@ -1,11 +1,26 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
 	render() {
 		return(
 			<div className="container">
-				<h1 id="second">Second Component</h1>
+				<h1 id="second">Hi { this.props.user.name }</h1>
+				<h2>{ this.props.user.college }</h2>
+				<div>
+					<h4>Hobbies</h4>
+					<ul>
+						{this.props.user.hobbies.map((hobby, i) => <li key = {i}>{ hobby }</li>)}
+					</ul>
+				</div>
+				<hr/>
+				{this.props.children}
 			</div>
 		);
 	}
 }
+
+Home.propTypes = {
+	user: PropTypes.object,
+	children: PropTypes.element.isRequired
+};
--- 1 Basics/src/app/index.js
+++ 1 Basics/src/app/index.js
@@ -8,13 +8,20 @@ import { Home } from "./components/Home"
 class App extends React.Component {
 	render() {
 		// what to do when component renders the code
+		var user = {
+			name: "Tejas",
+			college: "Atharva College of Engineering",
+			hobbies: ['Coding', 'Reading', 'Writing']
+		};
 		return(
 			<div className="container">
 				<div className="container">
 					<Header/>
 				</div>
 				<div className="container">
-					<Home/>
+					<Home user={user}>
+						<p>This is passed as children</p>
+					</Home>
 				</div>
 			</div>
 		);
