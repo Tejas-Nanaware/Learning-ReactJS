21dae37de51cc8623f476eaad57087c99a14c8a3
Tejas-Nanaware <nanawaret@hotmail.com>
2018-03-05 13:33:18 +0530

    Added list based reuse component

--- 2 More Basics/src/App.js
+++ 2 More Basics/src/App.js
@@ -5,32 +5,51 @@ class App extends Component {
 
   state = {
     persons: [
-      { name: 'abc', age: 11 },
-      { name: 'def', age: 3 },
-      { name: 'ghi', age: 12 }
+      { id: '1', name: 'abc', age: 11 },
+      { id: '2', name: 'def', age: 3 },
+      { id: '3', name: 'ghi', age: 12 }
     ],
     showDiv: false
   }
 
-  switchNameHandler = (newName) => {
+  // switchNameHandler = (newName) => {
+  //   this.setState( {
+  //     persons: [
+  //       { name: newName, age: 11 },
+  //       { name: 'def', age: 3 },
+  //       { name: 'ghi', age: 12 }
+  //     ]
+  //   } )
+  // }
 
-    this.setState( {
-      persons: [
-        { name: newName, age: 11 },
-        { name: 'def', age: 3 },
-        { name: 'ghi', age: 12 }
-      ]
-    } )
+  nameChangedHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} );
+
+    // this.setState( {
+    //   persons: [
+    //     { name: 'ABC', age: 11 },
+    //     { name: event.target.value, age: 3 },
+    //     { name: 'ghi', age: 12 }
+    //   ]
+    // } )
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'ABC', age: 11 },
-        { name: event.target.value, age: 3 },
-        { name: 'ghi', age: 12 }
-      ]
-    } )
+  deletePersonHandler = (PersonIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(PersonIndex, 1);
+    this.setState({persons: persons});
   }
 
   toggleDiv = () => this.setState({showDiv: !this.state.showDiv});
@@ -41,7 +60,15 @@ class App extends Component {
     if (this.state.showDiv === true) {
         persons = (
         <div>
-          <Person 
+          { this.state.persons.map((everyPerson, index) => {
+            return <Person 
+                          click={() => this.deletePersonHandler(index)}
+                          name={everyPerson.name} 
+                          age={everyPerson.age}
+                          key={everyPerson.id}
+                          changed={(event) => this.nameChangedHandler(event, everyPerson.id)} />
+          }) }
+          {/* <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age} />
           <Person 
@@ -51,7 +78,7 @@ class App extends Component {
             changed={this.nameChangedHandler} >My Hobbies: Coding</Person>
           <Person 
             name={this.state.persons[2].name} 
-            age={this.state.persons[2].age} />
+            age={this.state.persons[2].age} /> */}
         </div>);
     }
 
