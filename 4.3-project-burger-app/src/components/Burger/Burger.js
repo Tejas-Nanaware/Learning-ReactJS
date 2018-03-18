68d9a8df4d0b2ebf692b05424c10c5c9abe52d5b
Tejas-Nanaware <nanawaret@hotmail.com>
2018-03-18 12:25:30 +0530

    Bug fixes: empty list of ingredients

--- 4.3-project-burger-app/src/components/Burger/Burger.js
+++ 4.3-project-burger-app/src/components/Burger/Burger.js
@@ -3,12 +3,17 @@ import classes from './Burger.css';
 import Ingredients from './Ingredients/Ingredients';
 
 const Burger = (props) => {
-	const transformedIngredients = Object.keys(props.ingredients)
+	let transformedIngredients = Object.keys(props.ingredients)
 	.map(igkey => {
 		return [...Array(props.ingredients[igkey])].map((_, i) => {
 			return <Ingredients key = {igkey+i} type={igkey} />
 		});
-	});
+	}).reduce((arr, el) => {
+		return arr.concat(el)
+	}, []);
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Kindly add something!</p>
+	}
 	return(
 		<div className={classes.Burger}>
 			<Ingredients type='bread-top'/>
